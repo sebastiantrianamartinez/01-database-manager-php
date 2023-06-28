f9769bbe97f1738174d432a8ccb243ce08ba1a6d
Sebastian Triana martinez <84682654+sebastiantrianamartinez@users.noreply.github.com>
2023-06-28 12:21:47 -0500

    Add files via upload

--- core/lib/responser.php
+++ core/lib/responser.php
@@ -1,1 +1,31 @@
+<?php
+    define(ROOT, dirname(__FILE__, 3));
 
+    class responser {
+        public static function systemResponse($statusCode, $statusMessage, $data){
+            return [
+                "status" => $statusCode,
+                "message" => $statusMessage,
+                "data" => $data,
+                "signature" => "systemresponse"
+            ];
+        }
+        public static function preformedHttpResponse($objectResposne){
+            $objectResposne["signature"] = "httpresponse";
+            header('Content-Type: application/json; charset=utf-8');
+            http_response_code($objectResposne["status"]);
+            echo json_encode($objectResposne);
+        }
+    
+        public static function httpResponse($code, $message, $data){
+            $response = [
+                "status" => $code,
+                "message" => $message,
+                "data" => $data,
+                "signature" => "httpresponse"
+            ];
+            header('Content-Type: application/json; charset=utf-8');
+            http_response_code($code);
+            echo json_encode($response);
+        }
+    }
